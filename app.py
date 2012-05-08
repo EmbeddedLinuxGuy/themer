867350728415c71ff56ecf1f6265b02b36d3a010
jesse <jesse@pony.noisebridge.net>
2012-05-07 19:12:37 -0700

    add credentials

--- app.py
+++ app.py
@@ -4,20 +4,29 @@ import subprocess
 import os
 
 app = Flask(__name__)
+cred = {}
 
 @app.route('/')
 def index():
     sounds = ['alpha-beta.wav', 'gamma-delta.wav']
     return render_template('index.html', sounds=sounds) # send tuples
 
+@app.route('/test')
 def test():
-    [ user, password ] = get_cred
+    [ user, password ] = get_cred()
     cmd = [ 'sshpass', '-p', password, 'ssh',
             '-o', 'StrictHostKeyChecking=no',
             '-o', 'UserKnownHostsFile=/dev/null',
-            user, 'mpg123 chime.mp3' ]
-    p = subprocess.call(cmd)
+            user, 'ls' ]
+    p = subprocess.call(['ls'])
     return 'OK'
+
+def get_cred():
+    global cred
+    cred['user'] = os.environ['USER'] + '@horsy'
+    cred['password'] = os.environ['PASSWORD']
+
+    return [ cred['user'], cred['password'] ]
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug = True)
